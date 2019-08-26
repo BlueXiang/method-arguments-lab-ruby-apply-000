51a2859bbc454cafd7c53b652d11e344f71d5053
Xiang Jiang <thexiangjiang@gmail.com>
2019-08-26 07:09:33 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,4 +5,8 @@ end
 def introduction_with_language(name,language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
+<<<<<<< HEAD
    
+=======
+   
+>>>>>>> cebf77b89e602a6d6ec4b729700f9651599443cc
